ido50/waymux

commit 71554ad7e38216b2e51e28c9180e098dfb23c9c1

Put damage tracking debugging behind a flag

cage.c
@@ -10,6 +10,7 @@
 
 #include "config.h"
 
+#include <getopt.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -97,6 +98,42 @@ handle_signal(int signal, void *data)
 	}
 }
 
+static void
+usage(FILE *file, const char *cage)
+{
+	fprintf(file, "Usage: %s APPLICATION\n"
+		"\n"
+		" -D\t Turn on damage tracking debugging\n"
+		" -h\t Display this help message\n",
+		cage);
+}
+
+static bool
+parse_args(struct cg_server *server, int argc, char *argv[])
+{
+	int c;
+	while ((c = getopt(argc, argv, "hD")) != -1) {
+		switch (c) {
+		case 'D':
+			server->debug_damage_tracking = true;
+			break;
+		case 'h':
+			usage(stdout, argv[0]);
+			return false;
+		default:
+			usage(stderr, argv[0]);
+			return false;
+		}
+	}
+
+	if (optind == 0) {
+		usage(stderr, argv[0]);
+		return false;
+	}
+
+	return true;
+}
+
 int
 main(int argc, char *argv[])
 {
@@ -112,8 +149,7 @@ main(int argc, char *argv[])
 #endif
 	int ret = 0;
 
-	if (argc < 2) {
-		printf("Usage: %s APPLICATION\n", argv[0]);
+	if (!parse_args(&server, argc, argv)) {
 		return 1;
 	}
 
@@ -259,7 +295,7 @@ main(int argc, char *argv[])
 #endif
 
 	pid_t pid;
-	if (!spawn_primary_client(argv + 1, &pid)) {
+	if (!spawn_primary_client(argv + optind, &pid)) {
 		ret = 1;
 		goto end;
 	}

output.c
@@ -219,8 +219,9 @@ handle_output_damage_frame(struct wl_listener *listener, void *data)
 	}
 
 #ifdef DEBUG
-	// TODO: guard this behind a flag.
-	wlr_renderer_clear(renderer, (float[]){1, 0, 0, 1});
+	if (output->server->debug_damage_tracking) {
+		wlr_renderer_clear(renderer, (float[]){1, 0, 0, 1});
+	}
 #endif
 
 	float color[4] = {0.3, 0.3, 0.3, 1.0};
@@ -255,7 +256,9 @@ handle_output_damage_frame(struct wl_listener *listener, void *data)
 	wlr_renderer_end(renderer);
 
 #ifdef DEBUG
-	pixman_region32_union_rect(&damage, &damage, 0, 0, output_width, output_height);
+	if (output->server->debug_damage_tracking) {
+		pixman_region32_union_rect(&damage, &damage, 0, 0, output_width, output_height);
+	}
 #endif
 
 	enum wl_output_transform transform = wlr_output_transform_invert(output->wlr_output->transform);

server.h
@@ -36,6 +36,10 @@ struct cg_server {
 #if CAGE_HAS_XWAYLAND
 	struct wl_listener new_xwayland_surface;
 #endif
+
+#ifdef DEBUG
+	bool debug_damage_tracking;
+#endif
 };
 
 void set_window_title(struct cg_server *server, struct cg_view *view);

commit 3178396546bc0549a9eecf5c3a72cfca3295b253

Cage: exit when there is no application passed
This regression was introduced in 71554ad7e38216b2e51e28c9180e098dfb23c9c1
due to wrongfully handling optind.

cage.c
@@ -84,11 +84,13 @@ handle_signal(int signal, void *data)
 static void
 usage(FILE *file, const char *cage)
 {
-	fprintf(file, "Usage: %s APPLICATION\n"
+	fprintf(file, "Usage: %s [OPTIONS] [--] APPLICATION\n"
 		"\n"
 		" -d\t Don't draw client side decorations, when possible\n"
 		" -D\t Turn on damage tracking debugging\n"
-		" -h\t Display this help message\n",
+		" -h\t Display this help message\n"
+		"\n"
+		" Use -- when you want to pass arguments to APPLICATION\n",
 		cage);
 }
 
@@ -113,7 +115,7 @@ parse_args(struct cg_server *server, int argc, char *argv[])
 		}
 	}
 
-	if (optind == 0) {
+	if (optind >= argc) {
 		usage(stderr, argv[0]);
 		return false;
 	}